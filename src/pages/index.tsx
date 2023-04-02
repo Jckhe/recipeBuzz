6c84bd5de7ac8c29f62e7898a62e3ed646ad9285
jckhe <iamjckhe@gmail.com>
2023-04-01 18:15:52 -0700

    Working on hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
+import { Box, Stack, Heading, Text, Highlight } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hey</h1>
-    </div>
-  )
-};
+    <Box
+      borderColor="red.300"
+      borderWidth={1}
+      backgroundColor="#F0F0F0"
+      display="flex"
+      width="100%"
+      height="100vh"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Stack
+        direction="column"
+        width="40%"
+        marginTop={5}
+        borderColor="blackAlpha.200"
+        borderWidth={1}
+        backgroundColor="white"
+        textAlign="center"
+        px={7}
+        py={12}
+      >
+        <Heading size="lg">Unlock the World of Flavor.</Heading>
+        <Text>
+          <Highlight
+            query={["RecipeBuzz"]}
+            styles={{ px: "1", py: "1", rounded: "full", bg: "teal.100" }}
+          >
+            Discover a world of delicious and easy-to-make meals with
+            RecipeBuzz. Search and save your favorite recipes with ease and speed.
+          </Highlight>
+          <br/>
+          Get started now using the search bar below!
+        </Text>
+      </Stack>
+    </Box>
+  );
+}
