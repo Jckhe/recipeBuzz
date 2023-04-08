cba5d32785daf3672ee2334e7146ab921aaff0e4
jckhe <iamjckhe@gmail.com>
2023-04-07 21:35:49 -0700

    Fixed scrolling bg bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,44 @@
   import { ArrowForwardIcon } from "@chakra-ui/icons";
-  import { Stack, Heading, Highlight, Button, Box, Text } from "@chakra-ui/react";
+  import { Stack, Heading, Highlight, Button, Box, Text, } from "@chakra-ui/react";
   import Link from "next/link";
   import React from "react";
 
 
 //Hero component that is rendered in the main page.
-
 const Hero = React.memo(function Hero() {
   return (
     <Box
-    height="100vh"
-    width="100%"
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    borderBottom="1px solid black"
-    background={`url('https://images.unsplash.com/photo-1514986888952-8cd320577b68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80')  center center fixed`}
-    backgroundSize="cover"
+      height="100vh"
+      width="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      borderBottom="1px solid black"
+      background={{
+        'base': `url('/assets/Background.jpeg') center center scroll`,
+        'sm': `url('/assets/Background.jpeg') center center fixed`
+      }}
+      bgRepeat="no-repeat"
+      backgroundSize={{
+        "base": "fill",
+        "sm": "cover"
+      }}
     >
-        <Stack
-          direction="column"
-          width={{ 'base': '80%','md': '50%' }}
-          height={{ 'base': '35%', 'md': '50%'}}
-          marginTop={5}
-          borderColor="blackAlpha.200"
-          borderWidth={1}
-          backgroundColor="white"
-          textAlign="center"
-          display="flex"
-          justifyContent="center"
-          gap={1}
-          px={7}
-          py={12}
-          >
+      <Stack
+        direction="column"
+        width={{ base: "80%", md: "50%" }}
+        height={{ base: "35%", md: "50%" }}
+        marginTop={5}
+        borderColor="blackAlpha.200"
+        borderWidth={1}
+        backgroundColor="white"
+        textAlign="center"
+        display="flex"
+        justifyContent="center"
+        gap={1}
+        px={7}
+        py={12}
+      >
             <Heading
             fontFamily="EBGaramond08-Regular"
             size="lg"
